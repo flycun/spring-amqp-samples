f0a09289e7592ec374c5c24c8cad0eac45e7fae4
flycun <flycun@163.com>
2020-03-02 16:23:34 +0800

    Confirm Callback

--- spring-rabbit-ack/src/main/java/org/springframework/amqp/samples/ack/jobs/SendMessageJobs.java
+++ spring-rabbit-ack/src/main/java/org/springframework/amqp/samples/ack/jobs/SendMessageJobs.java
@@ -5,7 +5,9 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessagePropertiesBuilder;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.samples.ack.config.QueueConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,13 +20,15 @@ public class SendMessageJobs {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    private AtomicInteger count=new AtomicInteger();
+    private AtomicInteger count = new AtomicInteger();
 
     @Scheduled(fixedDelay = 2000)
-    public void sendMessage(){
+    public void sendMessage() {
         setupCallbacks();
-        rabbitTemplate.convertAndSend("fooDirectExchange","foo.key.1",new Foo("foo"));
-        log.info("send : {} ",count.incrementAndGet());
+        String json = "{\"foo\" : \"value\" }";
+        log.info("send : {} ", count.incrementAndGet());
+        rabbitTemplate.convertAndSend("fooDirectExchange", QueueConfig.FOO_ROUTER_KEY, json, new CorrelationData("" + count.get()));
+
     }
 
     private void setupCallbacks() {
@@ -35,33 +39,11 @@ public class SendMessageJobs {
             log.warn("Returned : {},replyCode: {},replyText: {},exchange/rk: {}/{}", message, replyCode, replyText, exchange, routingKey);
         });
 
-    }
-
-    public static class Foo {
-
-        private String foo;
-
-        public Foo() {
-            super();
-        }
-
-        public Foo(String foo) {
-            this.foo = foo;
-        }
-
-        public String getFoo() {
-            return this.foo;
-        }
-
-        public void setFoo(String foo) {
-            this.foo = foo;
-        }
-
-        @Override
-        public String toString() {
-            return "Foo [foo=" + this.foo + "]";
-        }
+        this.rabbitTemplate.setConfirmCallback((correlation, ack, reason) -> {
+            if (correlation != null) {
+                log.info("Received  {} for correlation: {},reason: {}", ack ? " ack " : " nack ", correlation, reason);
+            }
+        });
 
     }
-
 }
